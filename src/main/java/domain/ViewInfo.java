e999f7677f6878a309c27c75b93a47c3d1db3e2b
Liana Veldiaeva <lianali108@gmail.com>
2020-07-21 00:12:59 +0200

    another classes add v2#

---
+++ src/main/java/domain/ViewInfo.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class ViewInfo {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
+
