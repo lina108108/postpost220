c87a08818b6c35bba8d48634a7d9577754b31ba1
Liana Veldiaeva <lianali108@gmail.com>
2020-07-20 23:40:54 +0200

    basic files add

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,14 @@
+package domain;
+public class Post {
+    private String idNumber;
+    private String nameGroup;
+    private String date;
+    private int time;
+    private String content;
+    private String imageUrl;
+    private int likesInfo;
+    private int repostInfo;
+    private int viewInfo;
+    private int commentInfo;
+
+}
